036cfb81e54b0750bf9a371108605088b0a9d7f4
Maith3 <maithreyi@example.com>
2025-12-12 22:24:53 +0530

    Updating chatbot code

--- MaatruCare_ML/Chatbot2/main.py
+++ MaatruCare_ML/Chatbot2/main.py
@@ -3,10 +3,12 @@ from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_ollama import ChatOllama
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
 
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage],add_messages]
     
+checkpointer=MemorySaver()
 graph = StateGraph(ChatState)
 
 llm = ChatOllama(model="llama3.2:1b")
@@ -28,15 +30,19 @@ graph.add_node('chat_node',chat_node)
 graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node',END)
 
-chatbot = graph.compile()
+chatbot = graph.compile(checkpointer=checkpointer)
+
+thread_id = '1'
+
+config = {'configurable': {'thread_id': thread_id}}
 
 while True:
     user_message = input('You: ')
-    if user_message.strip().lower in ['quit','exit','bye']:
-        response=chatbot.invoke({'messages': [HumanMessage(content=user_message)]})
+    if user_message.strip().lower() in ['quit','exit','bye']:
+        response=chatbot.invoke({'messages': [HumanMessage(content=user_message)]},config=config)
         print('Bot: ',response['messages'][-1].content)
         break
-    response=chatbot.invoke({'messages': [HumanMessage(content=user_message)]})
+    response=chatbot.invoke({'messages': [HumanMessage(content=user_message)]},config=config)
     print('Bot: ',response['messages'][-1].content)
     
     
